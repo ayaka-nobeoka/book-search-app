84787ccfaee978a3252021a066b762ba285090f9
延岡あやか <nobeokaayaka@nobeokaayakanoMacBook-Air.local>
2025-05-05 18:55:22 +0900

    パスワード文字数チェック（例：6 文字以上）

--- book-search-app/src/pages/Login.jsx
+++ book-search-app/src/pages/Login.jsx
@@ -1,3 +1,67 @@
+import { useState } from "react";
+
 export default function Login() {
-  return <div>ログインページ</div>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorName, setErrorName] = useState("");
+  const [errorEmail, setErrormail] = useState("");
+  const [errorPassword, setErrorPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let isValid = true; //最初はすべて正しい（エラーなし）」という前提を決めている
+
+    if (!name) {
+      isValid = false;
+      setErrorName("名前を入力してください");
+    }
+    if (!email.includes("@")) {
+      isValid = false;
+      setErrormail("名前を入力してください");
+    }
+    if (password.length < 6) {
+      setErrorPassword("名前を入力してください");
+      isValid = false;
+    }
+    // すべてOKの場合
+    // ✅ 最後にまとめて判断
+    if (!isValid) return;
+    console.log("送信データ：", { name, email, password });
+  };
+  return (
+    <div>
+      <h2>ログインページ</h2>
+      <form onSubmit={handleSubmit}>
+        <p>ユーザー名</p>
+        <input value={name} onChange={(e) => setName(e.target.value)} />
+        {errorName && <p style={{ color: "red" }}>{setErrorName}</p>}
+        <p>メールアドレス</p>
+        <input value={email} onChange={(e) => setEmail(e.target.value)} />
+        {errorEmail && <p style={{ color: "red" }}>{setErrormail}</p>}
+        <p>パスワード</p>
+        <input value={password} onChange={(e) => setPassword(e.target.value)} />
+        {errorPassword && <p style={{ color: "red" }}>{setErrorPassword}</p>}
+        <button type="Submit">ログイン</button>
+      </form>
+    </div>
+  );
 }
+
+// ✖ setName(e)
+//    → name = { target: ..., value: ..., type: ..., ... }
+
+// ✔ setName(e.target.value)
+//    → name = "入力された文字"
+
+// ✅ !name の「!」って？
+// これは「否定」を意味する演算子（NOT）です。
+
+// !true → false
+
+// !false → true
+
+// !"" → true（空文字は falsy なので、それを否定すると true になる）
+
+// if (!name) は if (name === "" || name === null || name === undefined || name === false)
